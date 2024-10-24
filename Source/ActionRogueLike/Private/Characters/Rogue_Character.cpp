85de67f7edf33602ba861571d84cc4a165e07d8a
Wliered <wliered@proton.me>
2024-10-24 11:48:12 -0500

    Initialized Project
    - Added the Rogue_Character class and derived a BP version

---
+++ Source/ActionRogueLike/Private/Characters/Rogue_Character.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ActionRogueLike/Public/Characters/Rogue_Character.h"
+
+
+// Sets default values
+ARogue_Character::ARogue_Character()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ARogue_Character::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARogue_Character::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ARogue_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/ActionRogueLike/Public/Characters/Rogue_Character.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Rogue_Character.generated.h"
+
+UCLASS()
+class ACTIONROGUELIKE_API ARogue_Character : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ARogue_Character();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
