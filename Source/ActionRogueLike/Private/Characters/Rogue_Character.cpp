3f8a3fa1ed66420ebf9017c4bcae1393b226b2d0
Wliered <wliered@proton.me>
2024-10-26 14:44:51 -0500

    CharacterClass
    Created Character and set up inputs

--- Source/ActionRogueLike/Private/Characters/Rogue_Character.cpp
+++ Source/ActionRogueLike/Private/Characters/Rogue_Character.cpp
@@ -3,19 +3,47 @@
 
 #include "ActionRogueLike/Public/Characters/Rogue_Character.h"
 
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
 
 // Sets default values
 ARogue_Character::ARogue_Character()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	SpringArmComponent->SetupAttachment(GetRootComponent());
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>("Camera");
+	CameraComp->SetupAttachment(SpringArmComponent);
 }
 
 // Called when the game starts or when spawned
 void ARogue_Character::BeginPlay()
 {
 	Super::BeginPlay();
+
+	//Add Input Mapping Context
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
 	
+}
+
+void ARogue_Character::Move(const FInputActionValue& Value)
+{
+	//input is a Vector2D
+	FVector2D InputValue = Value.Get<FVector2D>();
+	AddMovementInput(GetActorForwardVector(), InputValue.X);
+	AddMovementInput(GetActorRightVector(), InputValue.Y);
 }
 
 // Called every frame
@@ -28,5 +56,12 @@ void ARogue_Character::Tick(float DeltaTime)
 void ARogue_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	//Setup Action Bindings
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		//Moving
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::Move);
+	}
 }
 
--- Source/ActionRogueLike/Public/Characters/Rogue_Character.h
+++ Source/ActionRogueLike/Public/Characters/Rogue_Character.h
@@ -3,8 +3,14 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputActionValue.h"
 #include "GameFramework/Character.h"
 #include "Rogue_Character.generated.h"
+
+class UInputAction;
+class UInputMappingContext;
+class USpringArmComponent;
+class UCameraComponent;
 
 UCLASS()
 class ACTIONROGUELIKE_API ARogue_Character : public ACharacter
@@ -15,9 +21,23 @@ public:
 	// Sets default values for this character's properties
 	ARogue_Character();
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* MoveAction;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* CameraComp;
+
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArmComponent;
+
+	void Move(const FInputActionValue& Value);
 
 public:
 	// Called every frame
